d205df7610226e05ed4f342a9c6f482b5606e205
ohsyme <49177554+ohsyme@users.noreply.github.com>
2023-02-18 15:45:19 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ export class Client {
   pup: PuppeteerExtra;
   pupBrowser?: Browser;
   opt: PuppeteerLaunchOptions;
-  constructor(pups:PuppeteerExtra, options:PuppeteerLaunchOptions={headless: false}) {
+  constructor(pups:PuppeteerExtra, options:PuppeteerLaunchOptions={headless: true}) {
     this.pup = pups;
     this.opt = options;
   }
